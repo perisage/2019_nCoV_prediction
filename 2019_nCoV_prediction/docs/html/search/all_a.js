a325f79c277fb821d0b45cc43c9931cd785e2d0f
PeripateticWind <1963127939@qq.com>
2020-02-13 11:38:36 +0800

    docs(docs): update docs
    update docs and some settings files comment

---
+++ 2019_nCoV_prediction/docs/html/search/all_a.js
@@ -0,0 +1,5 @@
+var searchData=
+[
+  ['the_202019_2dncov_20prediction_20application',['The 2019-nCoV Prediction Application',['../index.html',1,'']]],
+  ['todo_20list',['Todo List',['../todo.html',1,'']]]
+];
---
+++ 2019_nCoV_prediction/docs/html/search/all_b.js
@@ -0,0 +1,4 @@
+var searchData=
+[
+  ['write',['write',['../classCurveFittingEdge.html#a70bde4a6cbed5fc825a069c6f1f45caa',1,'CurveFittingEdge::write()'],['../classCurveFittingVertex.html#aa1443355babb211e227422ea0675dc32',1,'CurveFittingVertex::write()']]]
+];
